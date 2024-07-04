9d62262972db329f3a50e7cfca0f113e6d379cb3
John Mante <johnxmante@gmail.com>
2024-07-04 14:23:00 -0400

    wrapping app in MyProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { MyProvider } from './Context';
 import RaceTrackApp from './components/RaceTrackApp';
 
 function App() {
   return (
-    <div className="App">
-      <RaceTrackApp />
-    </div>
+    <MyProvider>
+      <div className="App">
+        <RaceTrackApp />
+      </div>
+    </MyProvider>
   );
 }
 
